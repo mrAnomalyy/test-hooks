bff4f061dd3264a5936291f46afddf7d87d8a05f
Bogdan Golubev <golubev@haulmont.com>
2020-12-07 13:52:13 +0400

    Initial counter commit; useState & useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState({ state: 0 });
+  const [switcher, setSwitcher] = useState({ state: true });
+  const [totalClicks, setTotalClicks] = useState(0);
+  const didMountRef = useRef(false);
+  useEffect(() => {
+    if (didMountRef.current) {
+      setTotalClicks((tc) => tc + 1);
+    } else {
+      didMountRef.current = true
+    }
+  }, [counter.state, switcher.state]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Your counter is { counter.state } now.
+      Increase it with <button onClick={() => setCounter({ state: counter.state + 1 })}>button</button>
+      <br />
+      <br />
+      Switch is { switcher.state ? "active" : "inactive" } now. 
+      Switch it's state with <button onClick={() => setSwitcher({ state: !switcher.state })}>button</button>
+      <br />
+      Total clicks on buttons: { totalClicks }      
     </div>
   );
 }
